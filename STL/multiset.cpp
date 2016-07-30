ba3617f5912af65e9f8b8e1dcfc3e14369572e09
Neha <nehaj1993@gmail.com>
2016-07-30 12:43:45 +0530

    std::multiset usage

---
+++ STL/multiset.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+void print_set(multiset<int> s)
+{
+  multiset<int>::iterator i;
+  cout << "Elements of the set : ";
+  for(i=s.begin(); i!=s.end();i++)
+    cout << *i << " ";
+  cout << endl;
+}
+
+int main()
+{
+  int n,i,x;
+  int arr[] = {1,2,3,4,5};
+
+  // Initialize the elements of set
+  multiset<int> input(arr, arr+5);
+  print_set(input);
+
+  // Erase element in the set
+  cout << "Erasing 4" << endl;
+  input.erase(4);
+  print_set(input);
+
+  // Search an element in the set
+  multiset<int>::iterator it;
+  it =  input.find(3);
+  cout << "Found : " << *it << endl;
+
+  // Size of the set
+  cout << "Size of the set is : " << input.size() << endl;
+
+  // Insert duplicate element in the set
+  input.insert(2);
+  print_set(input);
+
+  return 0;
+}
